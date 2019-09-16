1079954a81eebc7ba064cb7b78f2ad793d62e4b5
Shahrzad Rafezi <shahrzad_rafezi@yahoo.com>
2019-09-16 17:17:39 +1000

    added methods for players

--- Player.cpp
+++ Player.cpp
@@ -3,3 +3,47 @@
 //
 
 #include "Player.h"
+
+Player::Player(std::string playerName){
+
+}
+
+Player::Player(std::string playerName, int score, LinkedList hand){
+
+}
+
+Player::~Player(){
+
+}
+
+std::string Player::getName(){
+
+}
+
+int Player::getScore(){
+
+}
+
+void Player::addScore(){
+
+}
+
+bool Player::addTile(Tile* tile){
+
+}
+
+Tile Player::placeTile(std::string tileName){
+
+}
+
+std::string Player::playerScore(){
+
+}
+
+std::string Player::playerHand(){
+
+}
+
+std::string Player::toString(){
+
+}
--- Player.h
+++ Player.h
@@ -4,10 +4,39 @@
 
 #ifndef STARTER_CODE_PLAYER_H
 #define STARTER_CODE_PLAYER_H
-
+#include <string>
+#include "LinkedList.h"
 
 class Player {
 
+public:
+
+    Player(std::string playerName);
+
+    Player(std::string playerName, int score, LinkedList hand);
+
+    ~Player();
+
+    std::string getName();
+
+    int getScore();
+
+    void addScore();
+
+    bool addTile(Tile* tile);
+
+    Tile placeTile(std::string tileName);
+
+    std::string playerScore();
+
+    std::string playerHand();
+    
+    std::string toString();
+
+private:
+    std::string playerName;
+    int score;
+    LinkedList hand;
 };
 
 
