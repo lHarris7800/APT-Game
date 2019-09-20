edbe3fb8352590fab5e803a131065be8094f0b7b
Shahrzad Rafezi <shahrzad_rafezi@yahoo.com>
2019-09-21 09:26:07 +1000

    Fixed the Board issues

--- Board.h
+++ Board.h
@@ -1,24 +1,26 @@
 #ifndef APT_THE_SCREAM_A2_BOARD_H
 #define APT_THE_SCREAM_A2_BOARD_H
-
+#include<iostream>
 #include <string>
 #include "Tile.h"
-#define EMPTY_TILE '  '
+#define  MAX_SIZE 26
 
 class Board {
 public:
     Board();
-    void placePiece(Tile piece, std::string pos);
+
+    ~Board();
+
+//    void placePiece(Tile* piece, std::string pos);
+
     std::string displayBoard();
+
     std::string saveBoard();
-    bool canPieceBePlaced(Tile piece, std::string pos);
-    int rowToInt(char c);
+
+    bool canPieceBePlaced(Tile* piece, std::string pos);
 
 private:
-    int gridSize;
-    char row;
-    int col;
-    Tile board[26][26];
+    std::string board[MAX_SIZE][MAX_SIZE];
 };
 
 #endif //APT_THE_SCREAM_A2_BOARD_H
