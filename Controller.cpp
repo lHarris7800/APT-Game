9b01fb9cad967b230bdb810e07fbd290f012ecb1
Shahrzad Rafezi <shahrzad_rafezi@yahoo.com>
2019-10-05 17:38:57 +1000

    Updated validPlaceTile
    I had to check the surroundings to see if the tile can be placed if the shape/colour is similar.

--- Controller.cpp
+++ Controller.cpp
@@ -79,18 +79,56 @@ void Controller::playerChoice(){
     //todo figure out how to do this. enumerator?
 }
 
+
 bool Controller::validPlaceTile(Tile* playedTile, std::string boardLocation){
     int row, column;
     bool result = false;
+
     row = boardLocation[0]-65; //shows 1,2,3,....
+
+    //Converting Strings to Integers
     column = stoi(boardLocation.substr(1)); // shows A,B,C,...
 
-    //checks if the tile you want to place is out of bounds
-    if(row >= MAX_SIZE || column >= MAX_SIZE)
-        std::cout << "There are no more than 25 rows, Therefore you cannot add the tile in this position";
+    //checks if the tile that we are placing is inside of bounds, otherwise return false
+    if(row >= MAX_SIZE || column >= MAX_SIZE ){
+        std::cout << "There are no more than 25 rows, therefore you cannot add the tile in this position";
+        result = false;
+    }
 
-    //checks if the position of the board is empty
-    else if (board->board[column][row].compare("") == 0)
+    int blankNeighbour = 0;
+    int newRow, newCol;
+
+    //checks the surroundings of the tile
+    for(int dir = 1; dir <= 4; dir++){
+        newRow = row;
+        newCol = column;
+
+        if(dir == UP)
+            newCol++;
+
+        else if(dir == DOWN)
+            newCol--;
+
+        else if(dir == LEFT)
+            newRow--;
+
+        else
+            newRow++;
+
+        //Looks at the size of the board
+        if(newRow >= 0 && newRow < MAX_SIZE && newCol >= 0 && newCol < MAX_SIZE){
+            //looks at all 4 sides to see if the position is empty. if true, then add 1 to blank neighbour
+            if(board->board[newCol][newRow].compare("")==0)
+                blankNeighbour++;
+
+                //This is to check if the tile has the same shape or colour as the tile that's already in the board
+            else if(playedTile->getTileName()[0] == board->board[newCol][newRow][0] || playedTile->getTileName()[1] == board->board[newCol][newRow][1])
+                result = true;
+        }
+    }
+
+    //if all four tile's neighbour is empty, then place the tile.
+    if(blankNeighbour == 4)
         result = true;
     else
         std::cout << "There is a tile already in that position";
--- Controller.h
+++ Controller.h
@@ -1,8 +1,11 @@
 #include "Player.h"
 #include "Board.h"
-
 #include "Tile.h"
 #include "GameHistory.h"
+#define UP 1
+#define DOWN 2
+#define LEFT 3
+#define RIGHT 4
 
 class Controller {
 public:
