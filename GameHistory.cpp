1a5984271986f38c0cdc9b622ed6994c4769cc12
Luke <41284505+s3725331@users.noreply.github.com>
2019-10-09 17:43:02 +1100

    Bug fix for gameHistory save

--- GameHistory.cpp
+++ GameHistory.cpp
@@ -133,9 +133,9 @@ void GameHistory::setPlayerTwoUndos(int undos){
 std::string GameHistory::toString(){
     std::string build;
     build.append(iPlayerOne->toString() + "\n");
-    build.append(playerOneUndos + "\n");
+    build.append(std::to_string(playerOneUndos) + "\n");
     build.append(iPlayerTwo->toString() + "\n");
-    build.append(playerTwoUndos + "\n");
+    build.append(std::to_string(playerTwoUndos) + "\n");
     build.append(iBag->toString() + "\n");
 
     ActionNode* currAction = head;
